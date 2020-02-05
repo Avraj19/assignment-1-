64616f4898501af33e05a3fcc132a341806d31d2
Avraj Singh <ASingh@spartaglobal.com>
2020-02-05 23:19:14 +0000

    section 1

--- exercise_2_dictionary.py
+++ exercise_2_dictionary.py
@@ -1,7 +1,19 @@
 # section 1
-name = 'Jack'
-last_name = 'Chambers'
-age = 23
-age_of_mother = 48
-skill_list = ['fly', 'Control time', 'Make bubb;les']
+name = input('Enter your name: ').capitalize().strip()
+last_name = input('Enter your last name: ').capitalize().strip()
+age = int(input('Enter your age: '))
+age_of_mother = int(input('Enter your mothers age: '))
 
+
+# section 2 print out information in format manor?
+print(name)
+print(last_name)
+print(age)
+print(age_of_mother)
+# section 3
+age_dif = age - age_of_mother
+print(age_dif)
+# section 4 
+skill_list = ['fly', 'Control time', 'Make bubbles']
+
+# section 5
